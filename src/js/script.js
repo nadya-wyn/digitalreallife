d598c3f0259411abe41d56ccb98bbf9ec6d8a626
nadya wyn <nadya.wyn@gmail.com>
2021-08-12 12:44:03 +0500

    Added basic Mobile Navigation

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+	let //navContainer = document.querySelector('.header__nav-container'),
+		navList = document.querySelector('.header__nav'),
+		navToggle = document.querySelector('.header__nav-toggle');
+
+
+	navToggle.addEventListener('click', function () {
+		if (this.classList.contains('active')) {
+			this.classList.remove('active');
+			navList.classList.add('visually-hidden');
+			//navContainer.style.height = '140px';
+		} else {
+			//navContainer.style.height = 'auto';
+			this.classList.add('active');
+			navList.classList.remove('visually-hidden');
+
+		}
+
+	});
+
+});
