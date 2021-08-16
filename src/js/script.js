57ebdd61548bcabab7dfab5f993c434073265d50
nadya <nadya.wyn@gmail.com>
2021-08-16 10:39:31 +0500

    Added JS logic for Progress Items

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,38 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-	// let //navContainer = document.querySelector('.header__nav-container'),
-	// 	navList = document.querySelector('.header__nav'),
-	// 	navToggle = document.querySelector('.header__nav-toggle');
+	//Set variables
 
+	let progressIcon = document.querySelector('.progress'),
+		progressDiv = document.querySelector('.dashboard__list-item__progress'),
+		progressItems = document.querySelectorAll('.dashboard__list-item__progress-item');
 
-	// navToggle.addEventListener('click', function () {
-	// 	if (this.classList.contains('active')) {
-	// 		this.classList.remove('active');
-	// 		navList.classList.add('visually-hidden');
-	// 		//navContainer.style.height = '140px';
-	// 	} else {
-	// 		//navContainer.style.height = 'auto';
-	// 		this.classList.add('active');
-	// 		navList.classList.remove('visually-hidden');
+	//Progress Open-Close
+	progressIcon.addEventListener('click', function () {
+		if (this.classList.contains('progress-active')) {
+			this.classList.remove('progress-active');
+			this.classList.add('progress-close');
 
-	// 	}
+			progressDiv.classList.remove('visually-hidden');
+		} else {
+			this.classList.remove('progress-close');
+			this.classList.add('progress-active');
+			progressDiv.classList.add('visually-hidden');
+		}
+	});
 
-	// });
+	//Progress-Item active-inactive
+
+	function setItemColor(item) {
+		item.addEventListener('click', function () {
+			if (this.classList.contains('pi-active')) {
+				this.classList.remove('pi-active');
+			} else {
+				this.classList.add('pi-active');
+			}
+		});
+	}
+
+	progressItems.forEach(setItemColor);
+
 
 });
